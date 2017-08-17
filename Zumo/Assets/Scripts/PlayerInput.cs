64daba4983decd5c49e35ad879fc67625d6d409c
Jon Kelly <jon_k@live.ca>
2017-08-17 09:45:07 -0400

    First Commit!

---
+++ Zumo/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerInput : NetworkBehaviour
+{
+	// TODO : this is temporary
+	public SpriteRenderer PlayerImage;
+
+	void Start () 
+	{
+		
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		PlayerImage.color = Color.blue;
+	}
+
+	void Update () 
+	{
+		// TODO : for testing
+
+		if (!isLocalPlayer) // Only the local player should execute the movement code
+		{
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+	}
+}

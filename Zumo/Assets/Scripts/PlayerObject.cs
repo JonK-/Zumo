841f495b2b53717ecf8ce790ad69bb274f3cdbee
Jon Kelly <jon_k@live.ca>
2017-08-17 13:00:16 -0400

    Working on Score

--- Zumo/Assets/Scripts/PlayerObject.cs
+++ Zumo/Assets/Scripts/PlayerObject.cs
@@ -1,16 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerObject : MonoBehaviour 
+public class PlayerObject : NetworkBehaviour 
 {
+	public int PlayerScore = 0;
+
+	public SpriteRenderer PlayerImage;
+
+
 	void Start () 
 	{
-		
-	}
-	
-	void Update()
-	{
 
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		PlayerImage.color = Color.blue;
+	}
+
+	void Update () 
+	{
+		// TODO : for testing
+
+		if (!isLocalPlayer) // Only the local player should execute the movement code
+		{
+			return;
+		}
+
+		//		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		//		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		//		transform.Rotate(0, x, 0);
+		//		transform.Translate(0, 0, z);
+
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+			AddPlayerScore ();
+		} 
+		else if (Input.GetKeyDown (KeyCode.D)) 
+		{
+			SubtractPlayerScore ();
+		}
+	}
+
+	public void AddPlayerScore (int amount = 10)
+	{
+		PlayerScore += amount;
+	}
+
+	public void SubtractPlayerScore (int amount = 10)
+	{
+		PlayerScore -= amount;
 	}
 }
